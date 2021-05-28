2ab69ec62ff40bc7aa94b91c21170cbb416656a8
Emiliano Izquierdo <84947960+EmilianoIzquierdo@users.noreply.github.com>
2021-05-27 23:57:24 -0300

    Create ejercicio 2.ino

---
+++ Trabajo 2 27-05/ejercicio 2.ino
@@ -0,0 +1,32 @@
+int LEDV = 13;
+int pin = 3; 
+long tiempo1=0;
+long tiempo2=0; 
+int LEDR = 8;
+
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+attachInterrupt(digitalPinToInterrupt(pin), can, RISING);
+}
+void loop()
+{  
+ 
+}
+  
+
+           
+        
+           
+  void can(){
+    
+   tiempo1 = millis();
+  if((tiempo1-tiempo2)>2000){
+    tiempo2=tiempo1;
+ Serial.println("Interrupción activada");
+
+
+
+      
+  }
+}
+
